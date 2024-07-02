12ef70be838c766868986fe76e5c84905e2ab3bd
Rayyankkhan <ahmedkhankk247@gmail.com>
2024-07-02 17:20:17 +0500

    styles; fixes

--- src/Components/WeekMeals.jsx
+++ src/Components/WeekMeals.jsx
@@ -6,7 +6,7 @@ const WeekMeals = ({ meals, removeMeal }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-10 h-screen">
       {meals.map((meal, index) => (
-        <div key={index} className="">
+        <div key={index} className=" ">
           <MealCard
             mealname={meal.name}
             mealType={meal.mealType}
